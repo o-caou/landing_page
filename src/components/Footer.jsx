26174754d18ebae59f03abc1e7b2ae241c4125e3
Charleslmbrt <106771032+Charleslmbrt@users.noreply.github.com>
2024-10-15 21:51:33 +0200

    update anchor systeme footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,15 @@
 "use client";
 
-// Images
 import { useRef, useEffect } from "react";
 import Image from "next/image";
 import gsap from "gsap";
+import { useRouter, usePathname } from "next/navigation";
 import logoOcaou from "/public/image/logo-ocaou.svg";
 
 export default function Footer() {
   const footerLinksRef = useRef(null);
+  const router = useRouter();
+  const pathname = usePathname();
 
   useEffect(() => {
     if (footerLinksRef.current) {
@@ -38,51 +40,89 @@ export default function Footer() {
     }
   }, []);
 
+  const scrollToSection = (id) => {
+    const section = document.getElementById(id);
+    const headerHeight = 0;
+
+    if (section) {
+      const sectionPosition =
+        section.getBoundingClientRect().top + window.pageYOffset;
+
+      const offsetPosition = sectionPosition - headerHeight - 80;
+
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  const handleMenuItemClick = (id) => {
+    if (pathname === "/") {
+      scrollToSection(id);
+    } else {
+      router.push("/#" + id);
+    }
+  };
+
   return (
     <div className="w-full px-6 py-10 bg-sand sm:px-24">
       <div className="flex flex-col items-center sm:flex-row sm:justify-between sm:items-start">
         <Image src={logoOcaou} alt="logo O'CAOU" width={100} />
 
         <div
-          className="flex flex-col gap-10 text-sm text-center sm:text-left sm:gap-20 sm:flex-row"
+          className="flex flex-col gap-10 text-center sm:text-sm sm:text-left sm:gap-20 sm:flex-row"
           ref={footerLinksRef}
         >
-          <ul className="flex flex-col gap-y-2 mt-10">
+          <ul className="flex flex-col gap-y-2 mt-10 sm:mt-0">
             <li className="uppercase font-bold">La marque</li>
-            <li className="cursor-pointer underline-hover sm:w-fit">Le sac</li>
-            <li className="cursor-pointer underline-hover sm:w-fit">L'usage</li>
-            <li className="cursor-pointer underline-hover sm:w-fit">
+            <li
+              onClick={() => handleMenuItemClick("bags-content")}
+              className="cursor-pointer underline-hover sm:w-fit"
+            >
+              Le sac
+            </li>
+            <li
+              onClick={() => handleMenuItemClick("use-content")}
+              className="cursor-pointer underline-hover sm:w-fit"
+            >
+              L'usage
+            </li>
+            <li
+              onClick={() => handleMenuItemClick("spirit-content")}
+              className="cursor-pointer underline-hover sm:w-fit"
+            >
               L'esprit
             </li>
           </ul>
           <ul className="flex flex-col gap-y-2">
             <li className="uppercase font-bold">Acheter</li>
-            <li className="cursor-pointer underline-hover sm:w-fit">
+            <li className="cursor-pointer sm:underline-hover sm:w-fit">
               Particulier
             </li>
-            <li className="cursor-pointer underline-hover sm:w-fit">
+            <li className="cursor-pointer sm:underline-hover sm:w-fit">
               Revendeur
             </li>
           </ul>
           <ul className="flex flex-col gap-y-2">
             <li className="uppercase font-bold">Nos services</li>
-            <li className="cursor-pointer underline-hover sm:w-fit">FAQ</li>
-            <li className="cursor-pointer underline-hover sm:w-fit">
+            <li className="cursor-pointer sm:underline-hover sm:w-fit">FAQ</li>
+            <li className="cursor-pointer sm:underline-hover sm:w-fit">
               Livraisons & retours
             </li>
-            <li className="cursor-pointer underline-hover sm:w-fit">
+            <li className="cursor-pointer sm:underline-hover sm:w-fit">
               Où nous trouver ?
             </li>
           </ul>
           <ul className="flex flex-col gap-y-2">
             <li className="uppercase font-bold">Nous contacter</li>
-            <li className="cursor-pointer underline-hover sm:w-fit">
+            <li className="cursor-pointer sm:underline-hover sm:w-fit">
               Par mail
             </li>
-            <li className="cursor-pointer underline-hover sm:w-fit">
+            <li className="cursor-pointer sm:underline-hover sm:w-fit">
               Instagram
             </li>
-            <li className="cursor-pointer underline-hover sm:w-fit">
+            <li className="cursor-pointer sm:underline-hover sm:w-fit">
               Facebook
             </li>
           </ul>
